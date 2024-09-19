4ec6874fbc0e2071305f35b13d1f99ff3161ce8e
RenelBernadel <bernadelrenel@gmail.com>
2024-09-18 22:57:49 -0400

    Initial project source code

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import '@/app/ui/global.css'; // Import global styles
+import { inter } from '@/app/ui/fonts'; //Import the Inter font
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={'${inter.className} antialiased'}>{children}</body>
+    </html>
+  );
+}
